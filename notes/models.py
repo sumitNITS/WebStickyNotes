9f08d51d4f17df10f355e715288f062ab6de858f
Sumit Kumar Das <sumit.pradip@sifycorp.com>
2023-04-01 23:28:41 +0530

    added models for notes

---
+++ notes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Note(models.Model):
+    title = models.CharField(max_length=10000)
+    created_at = models.DateTimeField('Created', auto_now_add=True)
+    update_at = models.DateTimeField('Updated', auto_now=True)
+    isCompleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
