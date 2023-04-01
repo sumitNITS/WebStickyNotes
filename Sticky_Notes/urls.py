1473518ef757bdd176bc11c2383b322f296db461
Sumit Kumar Das <sumit.pradip@sifycorp.com>
2023-04-01 23:22:06 +0530

    added urls

---
+++ Sticky_Notes/urls.py
@@ -0,0 +1,16 @@
+"""
+URL Configuration
+https://docs.djangoproject.com/en/2.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings, views
+
+urlpatterns = [
+    path('notes/', include('notes.urls')),
+    path('admin/', admin.site.urls),
+    path('', views.index)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
